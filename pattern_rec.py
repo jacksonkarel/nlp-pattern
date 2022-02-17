184be70a2836e14becb3aebb80f94e1f1ec81dc9
Jackson Karel <jackson.karel2@gmail.com>
2022-02-17 04:54:21 -0800

    Add stop words, remove empty tuple, and save to csv

--- pattern_rec.py
+++ pattern_rec.py
@@ -2,12 +2,14 @@ import pickle
 from itertools import chain, combinations
 
 from tqdm import tqdm
+import pandas as pd
 
 with open('tokenized.p', 'rb') as token_file:
     tokenized = pickle.load(token_file)
 
 pattern_total = {}
 for txt in tqdm(tokenized):
+# txt = ['this', 'event', 'took', 'place', 'in', 'early', 'fall', 'around', '1949', '-', '50', '.', 'it', 'occurred', 'after', 'a', 'boy', 'scout', 'meeting', 'in', 'the', 'baptist', 'church', '.', 'the', 'baptist', 'church', 'sit']
     for txt_b in tokenized:
         if txt != txt_b:
             long_pat = []
@@ -16,13 +18,21 @@ for txt in tqdm(tokenized):
                     if t == tb:
                         long_pat.append(t)
                         break
-
-            powerset = chain.from_iterable(combinations(long_pat, r) for r in range(len(long_pat)+1))
-            powerlist = list(powerset)
-            for txt_pat in powerlist:
-                if txt_pat not in pattern_total:
-                    pattern_total[txt_pat] = 1
-                else:
-                    pattern_total[txt_pat] += 1
+            long_pat_len = len(long_pat)
+            long_pat_set = set(long_pat)
+            pat_stop_words = {"it", "in", "around", "this", "after"}
+            difference = long_pat_set - pat_stop_words
+            if len(difference) > 0:
+                powerset = chain.from_iterable(combinations(long_pat, r) for r in range(len(long_pat)+1))
+                powerlist = list(powerset)
+                powerlist.pop(0)
+                
+                for txt_pat in powerlist:
+                    if txt_pat not in pattern_total:
+                        pattern_total[txt_pat] = 1
+                    else:
+                        pattern_total[txt_pat] += 1
 
 pat_list = pattern_total.items()
+df = pd.DataFrame(pat_list)
+df.to_csv('patterns.csv')
