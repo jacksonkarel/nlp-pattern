32164ec28cae98b165ab32ba60c63021c1a8fe9d
Jackson Karel <jackson.karel2@gmail.com>
2022-04-02 18:04:43 -0700

    Create new kaggle versions

--- pattern_rec.py
+++ pattern_rec.py
@@ -3,14 +3,15 @@ from itertools import chain, combinations
 
 from tqdm import tqdm
 import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
 
 with open('tokenized.p', 'rb') as token_file:
     tokenized = pickle.load(token_file)
 
 pattern_total = {}
 for txt in tqdm(tokenized):
-# txt = ['this', 'event', 'took', 'place', 'in', 'early', 'fall', 'around', '1949', '-', '50', '.', 'it', 'occurred', 'after', 'a', 'boy', 'scout', 'meeting', 'in', 'the', 'baptist', 'church', '.', 'the', 'baptist', 'church', 'sit']
-    for txt_b in tokenized:
+    for txt_b in tqdm(tokenized):
         if txt != txt_b:
             long_pat = []
             for t in txt:
@@ -20,7 +21,7 @@ for txt in tqdm(tokenized):
                         break
             long_pat_len = len(long_pat)
             long_pat_set = set(long_pat)
-            pat_stop_words = {"it", "in", "around", "this", "after"}
+            pat_stop_words = {"it", "in", "around", "this", "after", "/"}
             difference = long_pat_set - pat_stop_words
             if len(difference) > 0:
                 powerset = chain.from_iterable(combinations(long_pat, r) for r in range(len(long_pat)+1))
@@ -35,4 +36,8 @@ for txt in tqdm(tokenized):
 
 pat_list = pattern_total.items()
 df = pd.DataFrame(pat_list)
-df.to_csv('patterns.csv')
+df.to_csv('patterns/patterns.csv')
+
+kaggle = KaggleApi()
+kaggle.authenticate()
+kaggle.dataset_create_version("patterns", "Generated from nlp-pattern script", convert_to_csv=False, delete_old_versions=True)
