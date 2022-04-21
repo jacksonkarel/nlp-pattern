2ebfc2eb27f95e52bd01c3bd0202a805f4cd140e
Jackson Karel <jackson.karel2@gmail.com>
2022-04-21 14:03:15 -0700

    Convert NUFORC comments to txt

--- tkenize/nuforc.py
+++ tkenize/nuforc.py
@@ -4,12 +4,16 @@ import pandas as pd
 
 from tkenize.helpers import txt_to_tokens
 
-def nuforc():
+def nuforc(txt=True):
     df_text = pd.read_csv('data/nuforc.csv', usecols=[7])
     df_text.dropna()
     df_text['comments'] = df_text['comments'].astype(str)
-    df_tokens = df_text["comments"].apply(txt_to_tokens)
-    tokenized = df_tokens.to_list()
-    tokenized.pop(0)
-    with open('tokenizations/nuforc.p', 'ab') as token_file:
-        pickle.dump(tokenized, token_file)
+    df_text['comments'] = df_text['comments'].str.strip()
+    if txt:
+        df_text.to_csv("sentences/nuforc.txt", header=None, index=None)
+    else:
+        df_tokens = df_text["comments"].apply(txt_to_tokens)
+        tokenized = df_tokens.to_list()
+        tokenized.pop(0)
+        with open('tokenizations/nuforc.p', 'ab') as token_file:
+            pickle.dump(tokenized, token_file)
