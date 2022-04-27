9ecc6f0ab12cef3da561f893e16e7da94b96774e
Jackson Karel <jackson.karel2@gmail.com>
2022-04-26 18:52:32 -0700

    Create dirs if they don't exist

--- segmenting/nuforc.py
+++ segmenting/nuforc.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 import pandas as pd
 
@@ -10,10 +11,12 @@ def segment_nuforc(txt=True):
     df_text['comments'] = df_text['comments'].astype(str)
     df_text['comments'] = df_text['comments'].str.strip()
     if txt:
+        os.makedirs("sentences", exist_ok=True)
         df_text.to_csv("sentences/nuforc.txt", header=None, index=None)
     else:
         df_tokens = df_text["comments"].apply(txt_to_tokens)
         tokenized = df_tokens.to_list()
         tokenized.pop(0)
+        os.makedirs("tokenizations", exist_ok=True)
         with open('tokenizations/nuforc.p', 'ab') as token_file:
             pickle.dump(tokenized, token_file)
