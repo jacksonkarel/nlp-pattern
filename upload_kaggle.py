937a1fb4304b08d15387503f6995226d1087e3ef
Jackson Karel <jackson.karel2@gmail.com>
2022-04-27 21:17:01 -0700

    Remove Kaggle uploader

--- upload_kaggle.py
+++
@@ -1,9 +0,0 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-from word_order import word_order
-
-pat_sw = ["it", "in", "around", "this", "after", "/"]
-word_order("tokenizations/tokenized.p", "patterns/nuforc/patterns.csv", pat_sw)
-kaggle = KaggleApi()
-kaggle.authenticate()
-kaggle.dataset_create_version("patterns/nuforc", "Generated from nlp-pattern script", convert_to_csv=False, delete_old_versions=True)
